108c4e4f1fddf4c04a77f6555c742d7ceb8764ee
sergey <sergey.liskonog@yandex.ru>
2014-12-08 07:18:37 +0400

    .

--- Tests/SiteTest.php
+++ Tests/SiteTest.php
@@ -1,12 +1,24 @@
 <?php
 
 use Madfox\WebCrawler\Site\Address;
+use Madfox\WebCrawler\Site;
 
-class SiteTest extends PHPUnit_Framework_TestCase {
+class SiteTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Site
+     */
+    private $site;
 
-    public function testCreateNewSite()
+    public function setUp()
     {
-          new \Buzz\Util\Url("/");
+        $this->site = new Site(new Address("http://foodbook.guru"));
+    }
+
+    public function testAddress()
+    {
+        $address = $this->site->getAddress();
+        $this->assertInstanceOf("Madfox\\WebCrawler\\Site\\AddressInterface", $address);
     }
 }
  
