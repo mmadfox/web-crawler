23777ff7c5f2583bd5b12722db1eb8fb1a53373d
sergey <sergey.liskonog@yandex.ru>
2014-12-08 08:13:31 +0400

    .

---
+++ Presets/PresetsCollection.php
@@ -0,0 +1,22 @@
+<?php
+namespace Madfox\WebCrawler\Presets;
+
+use Madfox\WebCrawler\Site;
+
+class PresetsCollection
+{
+    public function add()
+    {
+
+    }
+
+    public function install(Site $site)
+    {
+
+    }
+
+    public function uninstall(Site $site)
+    {
+
+    }
+}
