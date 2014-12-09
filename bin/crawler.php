b951eb75fca01ef475ab72ca00507a3cc070a62e
sergey <sergey.liskonog@yandex.ru>
2014-12-09 06:13:48 +0400

    .

---
+++ bin/crawler.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php
+require_once '../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Madfox\WebCrawler\Crawler;
+
+$console = new Application();
+
+$console
+    ->register('run')
+    ->setCode(function (InputInterface $input, OutputInterface $output) {
+          $output->writeln("Run....");
+          $crawler = new Crawler();
+          $crawler->site("http://ulkotours.com/")->ifpath("/")->exec();
+          $crawler->run();
+    });
+
+$console->run();
