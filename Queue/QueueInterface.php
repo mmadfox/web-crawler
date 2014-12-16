874bef79defa05fd4de3333d5414d7d06502a2d4
sergey <sergey.liskonog@yandex.ru>
2014-12-16 15:46:19 +0400

    Queue refactor

--- Queue/QueueInterface.php
+++ Queue/QueueInterface.php
@@ -1,29 +1,38 @@
 <?php
 namespace Madfox\WebCrawler\Queue;
 
-use Madfox\WebCrawler\Site\Url;
+use Madfox\WebCrawler\Url\Url;
 
 interface QueueInterface
 {
     /**
+     * @param string|int $channelName
+     * @return bool
+     */
+    public function registerChannel($channelName);
+    /**
      * @param Url $url
+     * @param string|int $channelName  optional
      * @return mixed
      */
-    public function enqueue(Url $url);
+    public function enqueue(Url $url, $channelName = null);
 
     /**
+     * @param string|int $channelName optional
      * @return Url|null
      */
-    public function dequeue();
+    public function dequeue($channelName = null);
 
     /**
+     * @param string|int $channelName optional
      * @return mixed
      */
-    public function purge();
+    public function purge($channelName = null);
 
     /**
      * @param Url $url
+     * @param string|int $channelName optional
      * @return mixed
      */
-    public function ack(Url $url);
+    public function ack(Url $url, $channelName = null);
 }
